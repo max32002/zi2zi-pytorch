ef875a9b0547a73b2f5f774d6ffa9ce9cec3d064
Max <max32002@gmail.com>
2025-03-24 12:38:35 +0800

    update font image conbiner

--- font_image_combiner.py
+++ font_image_combiner.py
@@ -26,21 +26,39 @@ def draw_character(char, font, canvas_size, x_offset=0, y_offset=0, auto_fit=Tru
         bbox = img.getbbox()
         if bbox is None:
             return None
-        left, upper, right, lower = bbox
-        left = max(0, left - 5)
-        upper = max(0, upper - 5)
-        right = min(canvas_size * 2 - 1, right + 5)
-        lower = min(canvas_size * 2 - 1, lower + 5)
-        if left >= right or upper >= lower:
+        l, u, r, d = bbox
+        l = max(0, l - 5)
+        u = max(0, u - 5)
+        r = min(canvas_size * 2 - 1, r + 5)
+        d = min(canvas_size * 2 - 1, d + 5)
+        if l >= r or u >= d:
             return None
-        img = np.array(img)[upper:lower, left:right]
+        img = np.array(img)
+        img = img[u:d, l:r]
         img = 255 - img
         img = Image.fromarray(img)
-        img = transforms.ToTensor()(img).unsqueeze(0)
-        pad_len = int(abs(img.shape[2] - img.shape[1]) / 2)
-        fill_area = (0, 0, pad_len, pad_len) if img.shape[2] > img.shape[1] else (pad_len, pad_len, 0, 0)
-        img = nn.ConstantPad2d(fill_area, 1)(img).squeeze(0)
-        img = transforms.ToPILImage()(img).resize((canvas_size, canvas_size), Image.BILINEAR)
+        # img.show()
+        width, height = img.size
+        # Convert PIL.Image to FloatTensor, scale from 0 to 1, 0 = black, 1 = white
+        try:
+            img = transforms.ToTensor()(img)
+        except SystemError:
+            return None
+        img = img.unsqueeze(0)  # 加轴
+        pad_len = int(abs(width - height) / 2)  # 预填充区域的大小
+        # 需要填充区域，如果宽大于高则上下填充，否则左右填充
+        if width > height:
+            fill_area = (0, 0, pad_len, pad_len)
+        else:
+            fill_area = (pad_len, pad_len, 0, 0)
+        # 填充像素常值
+        fill_value = 1
+        img = nn.ConstantPad2d(fill_area, fill_value)(img)
+        # img = nn.ZeroPad2d(m)(img) #直接填0
+        img = img.squeeze(0)  # 去轴
+        img = transforms.ToPILImage()(img)
+
+        img = img.resize((canvas_size, canvas_size), Image.BILINEAR)
     else:
         img = Image.new("RGB", (canvas_size, canvas_size), (255, 255, 255))
         draw = ImageDraw.Draw(img)
